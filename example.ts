30052ed39627a29601e4fad9867d1b4b4a77e5c3
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-11-01 17:50:34 +0100

    WIP: LwM2M full object typebox definition

--- example.ts
+++ example.ts
@@ -1,14 +1,17 @@
 import * as assert from "node:assert/strict";
 
-import { _5 as schema } from "./types/5";
+import { _5 } from "./types/5";
+import { _6 } from "./types/6";
+import { LwM2M } from "./LWM2M";
 import { validateWithJSONSchema } from "./src/utils/validateWithJsonSchema";
+import { Type } from "@sinclair/typebox";
 
 // This is a JSON representation of LwM2M, for example stored in AWS IoT Shadow
 // The JSON notation for LwM2M follows previous work by AVSystem as used in Coiote, but with values follow the LwM2M standard (e.g. numbers are expressed as strings in Coiote, but are Integers in LwM2M standard)
 // TODO: convert all strings to numbers where appropriate
 //import stateJSON from "./known-good-shadow.json";
 
-const example = {
+const _5Value: LwM2M.Object_5.Firmware_Update = {
   Name: "Firmware Update",
   ObjectURN: "urn:oma:lwm2m:oma:5:1.1",
   LWM2MVersion: 1.1,
@@ -23,10 +26,33 @@ const example = {
   },
 };
 
+const _6Value: LwM2M.Object_6.Location = {
+  Name: "Location",
+  ObjectURN: "urn:oma:lwm2m:oma:6",
+  LWM2MVersion: 1.1,
+  ObjectVersion: 1.1,
+  Resources: {
+    _0: "-43.5723",
+    _1: "153.21760",
+    _5: "10:25:54",
+  },
+};
+
+// TODO: create LwM2M full object typebox definition
+const LwM2MType = Type.Object({
+  _5, //Firmware_Update: Type.Object({ _5schema }),
+  _6, //Location: Type.Object({ _6schema }),
+});
+
+const shadow = {
+  _5: _5Value, // Firmware_Update
+  _6: _6Value, // Location
+};
+
 // We can validate that the data is correctly defined as LwM2M
 // TODO: define schema
-const validateLwM2MJSON = validateWithJSONSchema(schema);
-const maybeValidLwM2M = validateLwM2MJSON(example);
+const validateLwM2MJSON = validateWithJSONSchema(LwM2MType);
+const maybeValidLwM2M = validateLwM2MJSON(shadow);
 
 // Because is is know to be good, there must be no errors
 if ("error" in maybeValidLwM2M) throw new Error(`Validation failed`);
@@ -34,9 +60,9 @@ if ("error" in maybeValidLwM2M) throw new Error(`Validation failed`);
 // then we can access LwM2M objects in the shadow document
 
 assert.deepStrictEqual("value" in maybeValidLwM2M, true);
-assert.deepStrictEqual((maybeValidLwM2M as any).value.Resources._5, 10);
+assert.deepStrictEqual((maybeValidLwM2M as any).value._5.Resources._5, 10);
 
-console.log((maybeValidLwM2M as any).value.Resources._5); // 43200, typeof number
+console.log((maybeValidLwM2M as any).value._5.Resources._5); // 43200, typeof number
 
 // This ensuresure that the value is as we exect it
-assert.deepStrictEqual((maybeValidLwM2M as any).value.Resources._5, 10);
+assert.deepStrictEqual((maybeValidLwM2M as any).value._5.Resources._5, 10);
