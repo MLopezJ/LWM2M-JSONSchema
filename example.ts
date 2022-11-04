de1ad59e8726ac6b596e1ade78b3fedb38c86c79
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-11-04 12:35:20 +0100

    feat: create an example to library usage

--- example.ts
+++ example.ts
@@ -1,21 +1,25 @@
 import * as assert from "node:assert/strict";
-import { shadow } from "./exampleShadow";
+import { knowGoodShadow } from "./known-good-shadow";
 
 import { LwM2MType } from "./LWM2MType";
 import { validateWithJSONSchema } from "./src/utils/validateWithJsonSchema";
 
 // validate shadow
 const validateLwM2MJSON = validateWithJSONSchema(LwM2MType);
-const maybeValidLwM2M = validateLwM2MJSON(shadow);
+const maybeValidLwM2M = validateLwM2MJSON(knowGoodShadow);
 
 // Because is is know to be good, there must be no errors
 if ("error" in maybeValidLwM2M) throw new Error(`Validation failed`);
 
 // then we can access LwM2M objects in the shadow document
 assert.deepStrictEqual("value" in maybeValidLwM2M, true);
-assert.deepStrictEqual((maybeValidLwM2M as any).value._5.Resources._5, 10);
+const shadow: typeof LwM2MType = (maybeValidLwM2M as any).value;
 
-console.log((maybeValidLwM2M as any).value._5.Resources._5); // 43200, typeof number
+// Ensuresure values are as expected
+assert.deepStrictEqual(shadow._1.Resources._1, 43200);
+assert.deepStrictEqual(shadow._3.Resources._1, "thingy91_nrf9160");
+assert.deepStrictEqual(shadow._4.Resources._7, "ibasis.iot");
+assert.deepStrictEqual(shadow._3303.Resources._5701, "Celsius degrees");
+assert.deepStrictEqual(shadow._3323.Resources._5602, 98.24);
 
-// This ensuresure that the value is as we expect it
-assert.deepStrictEqual((maybeValidLwM2M as any).value._5.Resources._5, 10);
+console.log("Validation completed successfully");
