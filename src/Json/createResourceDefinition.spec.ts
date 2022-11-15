b61934e16d2fc43e6865a3390193b0e1f1930bc2
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-11-15 10:42:18 +0100

    test: check typebox def when RangeEnumeration format is invalid

--- src/Json/createResourceDefinition.spec.ts
+++ src/Json/createResourceDefinition.spec.ts
@@ -187,6 +187,30 @@ describe("createResourceDefinition", () => {
 
     expect(typeboxDefinition).toBe(result);
   });
+
+  it("Should check typebox definition when rangeEnumeration format is invalid", () => {
+    const name = "name";
+    const type = "Integer";
+    const description = "Description";
+    const mandatoryStatus = "Mandatory";
+    const multipleInstances = "Single";
+    const rangeEnumeration = "0..255 bytes";
+    const id = "16";
+    const units = "";
+    const typeboxDefinition = createResourceDefinition(
+      name,
+      type,
+      description,
+      mandatoryStatus,
+      multipleInstances,
+      rangeEnumeration,
+      id,
+      units
+    );
+    const result = `_16: Type.Number({title: 'name', description: "Description ... 0..255 bytes"})`;
+
+    expect(typeboxDefinition).toBe(result);
+  });
 });
 
 describe("createLiteralDefinition", () => {
